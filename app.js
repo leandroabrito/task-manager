395cec3d2f17faa30b7615473e6c0896bd0cb55b
leandro brito <leandroabrito89@gmail.com>
2020-06-23 17:55:15 -0300

    setup basic functionalities for tasks

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 const yargs = require('yargs');
+
+yargs.command({
+  command: 'add',
+  describe: 'add a task',
+  handler: function(){
+    console.log('adding a task');
+  }
+});
+
+yargs.command({
+  command: 'remove',
+  describe: 'remove a task',
+  handler: function(){
+    console.log('removing a task');
+  }
+});
+
+yargs.command({
+  command: 'list',
+  describe: 'list tasks',
+  handler: function(){
+    console.log('listing tasks');
+  }
+});
+
+yargs.command({
+  command: 'read',
+  describe: 'read a task',
+  handler: function(){
+    console.log('reading a task');
+  }
+});
+
+
+console.log(yargs.argv);
