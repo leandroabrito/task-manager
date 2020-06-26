1ff979c73d702d69270a0eefa0be004a547aa963
leandro brito <leandroabrito89@gmail.com>
2020-06-26 16:08:05 -0300

    create loadAllTasks method

--- task.js
+++ task.js
@@ -1,5 +1,20 @@
+const fs = require('fs');
+
 const addTask = function(title, description){
-  console.log(title, description)
+  const tasks = loadAllTasks()
+  console.log(tasks)
+}
+
+
+
+
+const loadAllTasks = function(){
+  try {
+    const tasks = fs.readFileSync('tasks.json');
+    return JSON.parse(tasks.toString());
+  } catch(error) {
+    return []
+  }
 }
 
 
